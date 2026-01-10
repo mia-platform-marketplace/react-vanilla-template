e2afa8e566fea64a11e4a61becdf07db74f09d07
Giulio Roggero <giulio.roggero@mia-platform.eu>
2026-01-10 18:39:43 +0100

    more simplification

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello from Mia Flow!</h1>
+      <h1>Hello from Mia Flow! v0.0.11</h1>
       <p>Just start to chat for create the application :-)</p>
     </div>
   )
